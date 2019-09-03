5df40e12fe5cdc9fe96e58836aa84afe98199b2d
yq010356 <yq010356@live.reading.ac.uk>
2019-09-03 17:01:54 +0100

    Carousel on home page, buttons on home, created product page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Jumbotron, Container, Row, Col, Image, Button } from 'react-bootstrap';
+import { Carousel, Container, Row, Col, Image, Button } from 'react-bootstrap';
 import './Home.css';
 
 // The home page for the website, will consist of a jumbotron
@@ -9,25 +9,100 @@ function Home() {
   return (
     // Creating a container to hold components
     <Container>
-        <Jumbotron>
-            <h2>Welcome</h2>
-            <p>This is a test, hello world</p>
-            <Link to="/products">
-                <Button variant="dark">Learn more</Button>
-            </Link>
-        </Jumbotron>
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="assets/carousel-image-1.jpg"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h2>Experience greatness..</h2>
+                    <div id="carousel-button">
+                        <Link to="/products">
+                            <Button variant="light">Shop now</Button>
+                        </Link>
+                    </div>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="assets/carousel-image-2.jpg"
+                    alt="Second slide"
+                />
+                <Carousel.Caption>
+                    <h2>High quality..</h2>
+                    <div id="carousel-button">
+                        <Link to="/products">
+                            <Button variant="light">Shop now</Button>
+                        </Link>
+                    </div>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="assets/carousel-image-3.jpg"
+                    alt="Third slide"
+                />
+                <Carousel.Caption>
+                    <h2>Great deals..</h2>
+                    <div id="carousel-button">
+                        <Link to="/products">
+                            <Button variant="light">Learn more</Button>
+                        </Link>
+                    </div>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
         <Row className="show-grid text-center">
-            <Col xs={6} sm={4} className="service-body">
+            <Col xs={12} sm={4} className="product-body">
+                <Link to="/products">
+                    <Button variant="link"><Image src="assets/115692.png" className="product-icon"></Image></Button>
+                </Link>
+                <h3>Men's Footwear</h3>
+                <div className="product-button">
+                    <Link to="/products">
+                        <Button variant="dark" size="lg">Shop now</Button>
+                    </Link>
+                </div>
+            </Col>
+            <Col xs={6} sm={4} className="product-body">
+                <Link to="/products">
+                    <Button variant="link"><Image src="assets/111998.png" className="product-icon"></Image></Button>
+                </Link>
+                <h3>Women's Footwear</h3>
+                <div className="product-button">
+                    <Link to="/products">
+                        <Button variant="dark" size="lg">Shop now</Button>
+                    </Link>
+                </div>
+            </Col>
+            <Col xs={6} sm={4} className="product-body">
+                <Link to="/products">
+                    <Button variant="link"><Image src="assets/77274.png" className="product-icon"></Image></Button>
+                </Link>
+                <h3>Child's Footwear</h3>
+                <div className="product-button">
+                    <Link to="/products">
+                        <Button variant="dark" size="lg">Shop now</Button>
+                    </Link>
+                </div>
+            </Col>
+        </Row>
+        <Row className="show-grid text-center">
+            <Col xs={12} sm={4} className="service-body">
                 <Image src="assets/quality.png" className="service-icon"></Image>
                 <h3>Quality Assured</h3>
                 <p>All shoes sold in-store or delivered will be in excellent quality.</p>
             </Col>
-            <Col xs={6} sm={4} className="service-body">
+            <Col xs={12} sm={4} className="service-body">
             <Image src="assets/shipping.png" className="service-icon"></Image>
                 <h3>Free Delivery</h3>
                 <p>All products sold include free standard delivery. The estimated time of standard delivery is one week.</p>
             </Col>
-            <Col xs={6} sm={4} className="service-body">
+            <Col xs={12} sm={4} className="service-body">
             <Image src="assets/gift.png" className="service-icon"></Image>
                 <h3>Gift Wrapping</h3>
                 <p>All shoes have the option to be gift wrapped at checkout.</p>
--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,10 +1,145 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Jumbotron, Container, Row, Col, Image, Button } from 'react-bootstrap';
+
+import './Products.css';
 
 function Products() {
+    
   return (
-    <div>
-        PRODUCTS PAGE
-    </div>
+    <Container>
+        <Jumbotron>
+            <div className="jumbo-heading">
+                <h5>Exclusive deals available!</h5>
+                <Button variant="light">Learn more</Button>
+            </div>
+        </Jumbotron>
+
+        <Row>
+            <Col xs={12} sm={3} className="filter-list">
+                <p>Filter</p>
+                
+            </Col>
+            <Col xs={12} sm={9} className="products-section">
+                <Row className="show-grid text-center">
+                    <Col xs={6} sm={4} className="products-page-body">
+                         <Link to="/products">
+                            <Button variant="link"><Image src="assets/115692.png" className="products-page-icon"></Image></Button>
+                        </Link>
+                        <div className="name-bar">
+                            <h5>Shoe Name</h5>
+                        </div>
+                        <h3>£79.99</h3>
+                    </Col>
+                    <Col xs={6} sm={4} className="products-page-body">
+                        <Link to="/products">
+                            <Button variant="link"><Image src="assets/111998.png" className="products-page-icon"></Image></Button>
+                        </Link>
+                        <div className="name-bar">
+                            <h5>Shoe Name</h5>
+                        </div>
+                        <h3>£59.99</h3>
+                    </Col>
+                    <Col xs={6} sm={4} className="products-page-body">
+                        <Link to="/products">
+                            <Button variant="link"><Image src="assets/77274.png" className="products-page-icon"></Image></Button>
+                        </Link>
+                        <div className="name-bar">
+                            <h5>Shoe Name</h5>
+                        </div>
+                        <h3>£49.99</h3>
+                    </Col>
+                </Row>
+                <Row className="show-grid text-center">
+                    <Col xs={6} sm={4} className="products-page-body">
+                         <Link to="/products">
+                            <Button variant="link"><Image src="assets/115692.png" className="products-page-icon"></Image></Button>
+                        </Link>
+                        <div className="name-bar">
+                            <h5>Shoe Name</h5>
+                        </div>
+                        <h3>£79.99</h3>
+                    </Col>
+                    <Col xs={6} sm={4} className="products-page-body">
+                        <Link to="/products">
+                            <Button variant="link"><Image src="assets/111998.png" className="products-page-icon"></Image></Button>
+                        </Link>
+                        <div className="name-bar">
+                            <h5>Shoe Name</h5>
+                        </div>
+                        <h3>£59.99</h3>
+                    </Col>
+                    <Col xs={6} sm={4} className="products-page-body">
+                        <Link to="/products">
+                            <Button variant="link"><Image src="assets/77274.png" className="products-page-icon"></Image></Button>
+                        </Link>
+                        <div className="name-bar">
+                            <h5>Shoe Name</h5>
+                        </div>
+                        <h3>£49.99</h3>
+                    </Col>
+                </Row>
+                <Row className="show-grid text-center">
+                    <Col xs={6} sm={4} className="products-page-body">
+                         <Link to="/products">
+                            <Button variant="link"><Image src="assets/115692.png" className="products-page-icon"></Image></Button>
+                        </Link>
+                        <div className="name-bar">
+                            <h5>Shoe Name</h5>
+                        </div>
+                        <h3>£79.99</h3>
+                    </Col>
+                    <Col xs={6} sm={4} className="products-page-body">
+                        <Link to="/products">
+                            <Button variant="link"><Image src="assets/111998.png" className="products-page-icon"></Image></Button>
+                        </Link>
+                        <div className="name-bar">
+                            <h5>Shoe Name</h5>
+                        </div>
+                        <h3>£59.99</h3>
+                    </Col>
+                    <Col xs={6} sm={4} className="products-page-body">
+                        <Link to="/products">
+                            <Button variant="link"><Image src="assets/77274.png" className="products-page-icon"></Image></Button>
+                        </Link>
+                        <div className="name-bar">
+                            <h5>Shoe Name</h5>
+                        </div>
+                        <h3>£49.99</h3>
+                    </Col>
+                </Row>
+                <Row className="show-grid text-center">
+                    <Col xs={6} sm={4} className="products-page-body">
+                         <Link to="/products">
+                            <Button variant="link"><Image src="assets/115692.png" className="products-page-icon"></Image></Button>
+                        </Link>
+                        <div className="name-bar">
+                            <h5>Shoe Name</h5>
+                        </div>
+                        <h3>£79.99</h3>
+                    </Col>
+                    <Col xs={6} sm={4} className="products-page-body">
+                        <Link to="/products">
+                            <Button variant="link"><Image src="assets/111998.png" className="products-page-icon"></Image></Button>
+                        </Link>
+                        <div className="name-bar">
+                            <h5>Shoe Name</h5>
+                        </div>
+                        <h3>£59.99</h3>
+                    </Col>
+                    <Col xs={6} sm={4} className="products-page-body">
+                        <Link to="/products">
+                            <Button variant="link"><Image src="assets/77274.png" className="products-page-icon"></Image></Button>
+                        </Link>
+                        <div className="name-bar">
+                            <h5>Shoe Name</h5>
+                        </div>
+                        <h3>£49.99</h3>
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
+    </Container>
   );
 }
 
